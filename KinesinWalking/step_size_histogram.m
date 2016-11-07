5d0a6f0e19f3020f44f2eb897b42a1cadd4efd62
Saurabh Shukla <sshukla101@users.noreply.github.com>
2016-11-06 23:31:16 -0600

    Update step_size_histogram.m

--- KinesinWalking/step_size_histogram.m
+++ KinesinWalking/step_size_histogram.m
@@ -25,3 +25,39 @@
     ylabel('frequency')
 
     
+%% Finding out instantaneous velocity
+    exposureTime=0.00788;
+    CodePath=pwd;
+    DataPath=uigetdir;
+    cd(DataPath);
+    FileIn=dir('*.txt');
+    for i=1:length(FileIn)
+        fid=fopen(FileIn(i).name);
+        Input = textscan(fid,'%f%f','CommentStyle','##');
+        yInput = Input{1};
+        position=yInput(1:2:end);
+        x=yInput(2:2:end);
+        plot(x,position);
+        
+        %Input start and end position for instaneous velocity
+        prompt = 'Start position';
+        start_l = input(prompt);
+        prompt = 'End position';
+        end_l = input(prompt);
+        
+        v(i)=abs((position(start_l) - position(end_l))/ ((start_l-end_l)*exposureTime));
+    end
+    
+    v_avg=mean(v);
+    v_max=max(v);
+    v_min=min(v);
+    
+     
+        
+        
+        
+        
+        
+        
+            
+    
